f3f4763f3b974ad9f8d1228ebdfcd46222a3b9ce
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2022-10-24 17:15:50 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/gvfs/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.gvfs.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gvfs.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
--- src/main/java/com/gvfs/course/repositories/UserRepository.java
+++ src/main/java/com/gvfs/course/repositories/UserRepository.java
@@ -6,4 +6,5 @@ import com.gvfs.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
 }

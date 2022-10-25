48b4da29caf1182022fb3d78274358ba1bfe03e0
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2022-10-24 23:13:22 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/gvfs/course/repositories/ProductRepository.java
+++ src/main/java/com/gvfs/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.gvfs.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.gvfs.course.entities.Category;
 import com.gvfs.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
